9abd7cb1018ea5878e9a4f4c54054693e0b33c5d
free jacow gwn <jakovmarkovanovic@gmail.com>
2019-06-01 18:21:54 +0200

    Add files via upload

---
+++ PRS/Core/Network/Gateway.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Royale2Sharp.Core.Settings;
+using Royale2Sharp.Packets;
+using Royale2Sharp.Packets.Crypto.RC4;
+
+namespace Royale2Sharp.Core.Network
+{
+    // State object for reading client data asynchronously
+    public class StateObject
+    {
+        // Size of receive buffer.
+        public const int BufferSize = 4096;
+
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+
+        // Client  socket.
+        public Socket workSocket;
+    }
+
+    public class AsynchronousSocketListener
+    {
+        // Thread signal.
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.
+            var bytes = new byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // The DNS name of the computer
+            // running the listener is "host.contoso.com".
+            var ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            var ipAddress = ipHostInfo.AddressList[0];
+            var localEndPoint = new IPEndPoint(ipAddress, 9339);
+
+            // Create a TCP/IP socket.
+            var listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+                Program.elpst.Stop();
+                Logger.Log($"HuzaModz Royale Server Started at {listener.LocalEndPoint} in {Program.elpst.ElapsedMilliseconds}ms!",
+                    Logger.DefCon.INFO);
+                while (true)
+                {
+                    // Set the event to nonsignaled state.
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.
+
+                    listener.BeginAccept(
+                        AcceptCallback,
+                        listener);
+
+                    // Wait until a connection is made before continuing.
+                    allDone.WaitOne();
+                    Message.RC4 = new RC4_Core();
+                    Logger.Log($"Someone Connected YAY!", Logger.DefCon.INFO);
+                    Constants.ServerConfig.ConnectedUsers++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.
+            allDone.Set();
+
+            // Get the socket that handles the client request.
+            var listener = (Socket) ar.AsyncState;
+            var handler = listener.EndAccept(ar);
+
+            // Create the state object.
+            var state = new StateObject();
+            state.workSocket = handler;
+            state.buffer = new byte[9999];
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                ReadCallback, state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            var state = (StateObject) ar.AsyncState;
+            var handler = state.workSocket;
+
+            try
+            {
+                var bytesRead = handler.EndReceive(ar);
+
+                if (bytesRead > 0)
+                    if (handler.Connected)
+                    {
+                        new Message(handler, state.buffer);
+                        state.buffer = new byte[9999];
+                        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                            ReadCallback, state);
+                    }
+                    else
+                    {
+                        Logger.Log($"Asshole {handler.RemoteEndPoint} disconnected from the server!",
+                            Logger.DefCon.WARN);
+                    }
+                else
+                    Logger.Log($"Asshole {handler.RemoteEndPoint} disconnected from the server!", Logger.DefCon.WARN);
+            }
+            catch (Exception e)
+            {
+                Logger.Log("We threw an exception receiving the data from the client : " + e.Message,
+                    Logger.DefCon.ERROR);
+            }
+        }
+
+        private static void Send(Socket handler, byte[] packet)
+        {
+            handler.BeginSend(packet, 0, packet.Length, 0,
+                SendCallback, handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                var handler = (Socket) ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                var bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ PRS/Core/Settings/Constants.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+
+namespace Royale2Sharp.Core.Settings
+{
+    internal class Constants
+    {
+        internal static Random r = new Random();
+
+        internal class ServerConfig
+        {
+            public const string Ip = "192.168.100.8";
+            public const int Port = 9339;
+            public const bool IsMaintenance = false;
+            public static readonly string AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            public static readonly List<string> BlockedIps = new List<string>
+            {
+                ""
+            };
+
+            public static byte[] Decrypted = new byte[2048];
+            public static byte[] Encrypted = new byte[2048];
+            public static byte[] ReceivedPacket = new byte[2048];
+
+            public static int ContentTimes = 0;
+
+            public static int ConnectedUsers = 0;
+        }
+
+        internal class ProtocolConfig
+        {
+            public const bool patchClient = true;
+
+            public const bool IsMaintenance = false;
+
+            public static byte[] Decrypted = new byte[2048];
+
+            public static byte[] Encrypted = new byte[2048];
+
+            public static byte[] ReceivedPacket = new byte[2048];
+
+            public static int ContentTimes = 0;
+        }
+
+        internal class ServerHello
+        {
+            public const string SessionKey = "PrOjEcTrOyAlEsErVeR-ClAsHrOyAlE12345";
+        }
+
+        internal class LoginFailed
+        {
+            public enum Error
+            {
+                Default = 0,
+                OutDatedContent = 7,
+                Update = 8,
+                Redirect = 9,
+                Maintenance = 10,
+                Banned = 11,
+                Locked = 13
+            }
+
+            public const string Redirect =
+                "stage.scroll.supercelltest.com";
+
+            public const string ContentUrl =
+                    "http://192.168.1.2/ClashRoyale/Patchs"
+                ; //https://99faf1e355c749a9a049-2a63f4436c967aa7d355061bd0d924a1.ssl.cf1.rackcdn.com 
+
+            public const string AssetsUrl =
+                "https://game-assets.clashroyaleapp.com";
+
+            public const string UpdateUrl =
+                "https://play.google.com/store/apps/details?id=com.supercell.clashroyale";
+
+            public const int MaintenanceLeftSeconds = 0;
+
+            public static string fingerprintVersion;
+
+            public static string FingerPrint;
+                
+
+
+            public const string Message = "Welcome To Huza Modz Royale server";
+        }
+
+        internal class LoginOk
+        {
+            public const uint Id = 4033384339;
+
+            public const string City = "ProjectLand | Italy";
+
+            public const string Contenturl =
+                "http://7166046b142482e67b30-2a63f4436c967aa7d355061bd0d924a1.r65.cf1.rackcdn.com";
+
+            public const string Eventsurl = "https://image.prntscr.com/image";
+        }
+
+        internal static class OwnHomeData
+        {
+            public static int[] Deck = {1, 2, 3, 4, 5, 6, 64, 30};
+
+            public const string StartingPlayerName = "Huza Modz";
+
+            public const string StartingClanName = "Huza Modz";
+
+            public const int StartingLevel = 13;
+
+            public const int StartingExperience = 1000000;
+
+            public const int StartingGems = 100000;
+
+            public const int StartingGold = 100000;
+
+            public static readonly int StartingTrophies = 6400; //r.Next(0, 6900);
+        }
+
+        internal class AllianceData
+        {
+            public const string Description = "Official royale server HuzaModz";
+        }
+        internal class AllianceMessage
+        {
+            public static long id = 0;
+        }
+    }
+}
