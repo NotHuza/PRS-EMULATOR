fe659d7b0e459f3af536c98b0a87d64b19b48453
free jacow gwn <jakovmarkovanovic@gmail.com>
2019-06-01 18:27:50 +0200

    Add files via upload

---
+++ PRS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Colorful;
+using Royale2Sharp.Core;
+using Royale2Sharp.Core.Network;
+using Royale2Sharp.Core.Settings;
+
+namespace Royale2Sharp
+{
+    internal class Program
+    {
+        internal static Stopwatch elpst = new Stopwatch();
+
+        private static void Main(string[] args)
+        {
+            Console.Write(@"
+            
+              █  █ █  █  ▀▀█  █▀█  █▀▄▀█ █▀▀█ █▀▀▄ ▀▀█   █▀▀█ █▀▀█ █  █ █▀▀█ █   █▀▀   █▀█   █▀▀█
+              █▄▄█ █  █  ▄▀  █▀▀▀█ █ ▀ █ █  █ █  █ ▄▀    █▄▄▀ █  █ █▄▄█ █▄▄█ █   █▀▀    ▄▀ ▄ █▄▀█
+              █  █  ▀▀▀  ▀▀  █   █ ▀   ▀ ▀▀▀▀ ▀▀▀  ▀▀▀   ▀ ▀▀ ▀▀▀▀ ▄▄▄█ ▀  ▀ ▀▀▀ ▀▀▀   █▄▄ █ █▄▄█ v." +
+                                             Constants.ServerConfig.AppVersion, Color.Crimson);
+            Console.WriteLine();
+
+            elpst.Reset();
+
+            elpst.Start();
+
+            Logger.Log($"Starting...", Logger.DefCon.INFO);
+
+            AsynchronousSocketListener.StartListening();
+
+            Console.ReadKey(true);
+        }
+    }
+}
